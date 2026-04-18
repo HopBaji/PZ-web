a9f77a8def52dbaa6b505f3fba378389965b5310
HopBaji <198099724+HopBaji@users.noreply.github.com>
2026-04-18 21:18:40 +0300

    Vr1

---
+++ demo/src/main/java/com/example/demo/Controller/controller.java
@@ -0,0 +1,19 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+
+@Controller
+public class controller {
+    @RequestMapping("/")
+    public String Main() {
+        return "index";
+    }
+    
+    
+}
